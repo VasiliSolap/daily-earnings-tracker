1f27da8088ad97710420c4b23dd493fbe5921812
Vasili <vasilisolap@gmail.com>
2025-10-31 13:17:55 -0700

    no
    Updated daily report logic

---
+++ daily_report.py
@@ -0,0 +1,106 @@
+# 📍 Location selection
+bonus_rate = 0
+location_name = ''
+
+location_choice = int(input('Which city do you work in? (1-Seattle, 2-Lynnwood, 3-Bellevue): '))
+
+if location_choice == 1:
+    location_name = 'Seattle'
+    bonus_rate = 10
+elif location_choice == 2:
+    location_name = 'Lynnwood'
+    bonus_rate = 5
+elif location_choice == 3:
+    location_name = 'Bellevue'
+    bonus_rate = 15
+else:
+    location_name = 'N/A'
+    bonus_rate = 0
+
+# 📦 Work stats
+while True:
+	try:
+		orders = int(input('How many orders?: '))
+		if orders < 0:
+			print("❌ Orders can't be negative. Try again.")
+			continue
+		break
+	except ValueError:
+		print("❌ Enter a whole number.")
+	
+while True:
+	try:
+		price_per_order = float(input('Average price per order?: '))
+		if price_per_order < 0:
+			print("❌Price can't be negative. Try again")
+			continue
+		break
+	except ValueError:
+		print("❌ Enter a whole number.")
+
+while True:		
+		try:
+			hours_worked = float(input('How many hours did you work?: '))
+			if hours_worked < 0:
+				print("❌Hours can't be negative. Try again")
+				continue
+			break
+		except ValueError:
+			print("❌ Enter a whole number.")
+
+# 💵 Base earnings
+base_earnings = orders * price_per_order
+
+# 🌆 Location bonus
+bonus_multiplier = bonus_rate / 100
+bonus_amount = base_earnings * bonus_multiplier
+earnings_after_bonus = base_earnings + bonus_amount
+
+# 💸 Tax selection
+while True:
+	try:
+		tax_rate = float(input('What is your tax percentage? (1–50): '))
+		if tax_rate <= 0 or tax_rate > 50:
+			print("❌ Wrong! Enter number from 1 to 50.")
+			continue
+		break
+		
+	except ValueError:
+		print("❌ Wrong! Enter valid number, for example: 10, 15 or 20.")
+
+
+# 📊 Calculations
+tax_value = earnings_after_bonus * (tax_rate / 100)
+net_earnings = earnings_after_bonus - tax_value
+earnings_per_hour = net_earnings / hours_worked
+
+# ✅ Final report
+print(f"\n📍 Location: {location_name} (+{bonus_rate}%)")
+print(f"💵 Total earnings before tax: ${earnings_after_bonus:.2f}")
+print(f"💸 Tax ({tax_rate}%): -${tax_value:.2f}")
+print(f"💰 Net earnings: ${net_earnings:.2f}")
+print(f"⏱ Earnings per hour: ${earnings_per_hour:.2f}")
+
+from datetime import datetime
+today = datetime.now().strftime("%Y-%m-%d")
+
+with open("daily_report.txt", "a") as file:
+		file.write('\n')
+		file.write(f'==={today}===\n')
+		file.write(f"Location: {location_name} (+{bonus_rate}%)\n")
+		file.write(f"Total before tax: ${earnings_after_bonus:.2f}\n")
+		file.write(f"Tax ({tax_rate}%): -${tax_value:.2f}\n")
+		file.write(f"Net earnings: ${net_earnings:.2f}\n")
+		file.write(f"Earnings per hour: ${earnings_per_hour:.2f}\n")
+		
+with open("daily_report.txt", "r") as file:
+	lines = file.readlines()
+	
+	
+if len(lines)<20: 
+	recent_lines=lines
+else:
+	recent_lines=lines[-20:]
+	
+numbered = [f'{i}:{line.strip()}' for i, line in enumerate(recent_lines, start=1)]
+print("\n".join(numbered))
